f44efd8cc6dd1d78aac70cf50af6a7f53b229388
Shubham <shubhampandey2029@gmail.com>
2024-08-17 01:17:42 +0530

    Adding src folder

---
+++ client/src/pages/About.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function About() {
+  return (
+    <div className="flex items-center justify-center h-screen">
+  <p>"Our Recommender System works on a hybrid model utilising both content based filtering and collaborative based filtering."</p>
+</div>
+
+  )
+}
---
+++ client/src/redux/store.js
@@ -0,0 +1,26 @@
+import {combineReducers, configureStore } from '@reduxjs/toolkit';
+import userReducer from './user/userSlice.js';
+import { persistReducer, persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+//We have used reducer from react redux library to store the info of the user even after he refreshes the page
+
+const rootReducer = combineReducers({ user: userReducer });
+
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: false,
+    }),
+});
+
+export const persistor = persistStore(store);
