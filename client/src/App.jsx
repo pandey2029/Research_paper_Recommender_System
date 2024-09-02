b8d563057a7f99ef86dc1e1205553f84fd0d3579
pandey2029 <107745010+pandey2029@users.noreply.github.com>
2024-09-02 20:14:53 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -12,11 +12,12 @@ export default function App() {
     <BrowserRouter>
     <Header/>
       <Routes>
-        <Route path="/" element={<Home/>} />
+       
         <Route path="/about" element={<About/>} />
         <Route path="/sign-in" element={<SignIn/>} />
         <Route path="/sign-up" element={<SignUp/>} />
         <Route element={<PrivateRoute />}>
+           <Route path="/" element={<Home/>} />
           <Route path='/profile' element={<Profile />} />
         </Route>
       </Routes>
